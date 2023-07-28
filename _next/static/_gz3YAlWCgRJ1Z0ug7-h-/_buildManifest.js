dcc224e2c82a97e39bb48458cb170580bf96c96f
ggdaltoso <ggdaltoso@users.noreply.github.com>
2023-07-28 19:49:47 +0000

    deploy: 2fad28084353f7ed4efe5d9a1e855b89c4cf97d0

--- _next/static/_gz3YAlWCgRJ1Z0ug7-h-/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/890-eae7ab42644c847a.js","static/chunks/pages/index-ee504a5f511e5ae0.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

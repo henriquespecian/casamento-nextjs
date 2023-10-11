b5dfcea769c23fd3cf6b7bced09bc609258e0fd3
henriquespecian <henriquespecian@users.noreply.github.com>
2023-10-11 22:11:17 +0000

    deploy: 814fa38f2e89178075246b67bae5e195c09a3c54

--- _next/static/_bJbGLbj2pg1ZVhZishfy/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/232-3bd036ba8fe2d49c.js","static/chunks/pages/index-c6470fee818287c2.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
---
+++ _next/static/_hwoZpyz3g_C1ykctfV8q/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/270-6fe928af6005ae90.js","static/css/3d14283a3f96d9db.css","static/chunks/pages/index-920baedecd34ab1c.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

799422c88cd66c11a743016b793ee55e782511ee
henriquespecian <henriquespecian@users.noreply.github.com>
2023-10-25 20:07:15 +0000

    deploy: 91c7ea88a53723eed8a004469ad6899a3bf2d68a

---
+++ _next/static/h0WSHZrNVtolgcX8SV3Ie/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/270-52f472b182cd5b61.js","static/css/3d14283a3f96d9db.css","static/chunks/pages/index-8a823664405c9d42.js"],"/_error":["static/chunks/pages/_error-effe22be6ff34abe.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
